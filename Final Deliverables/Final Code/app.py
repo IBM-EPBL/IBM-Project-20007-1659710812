d146a238b6916644f3356723b14c9ac56e0b9e92
Madhuvanthi <madhuvanthivenkat@gmail.com>
2022-11-16 22:51:52 +0530

    Making changes

--- Final Deliverables/Final Code/app.py
+++ Final Deliverables/Final Code/app.py
@@ -1,13 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  4 14:19:28 2022
-
-@author: Mr...Vs..99
-"""
-
 from flask import Flask,render_template,request
-# Flask-It is our framework which we are going to use to run/serve our application.
-#request-for accessing file which was uploaded by the user on our application.
 import os
 import numpy as np #used for numerical analysis
 from tensorflow.keras.models import load_model#to load our trained model
